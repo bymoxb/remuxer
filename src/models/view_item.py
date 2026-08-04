f888ab8ad05938f3d259c4939966c8a089c728b8
Luis Fernando Illapa <moxb@outlook.es>
2026-08-03 23:04:40 -0500

    add formater class

--- src/models/view_item.py
+++ src/models/view_item.py
@@ -1,5 +1,8 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
+from decimal import Decimal, ROUND_HALF_UP
+
+
 from gi.repository import GObject
 
 
@@ -30,7 +33,10 @@ class StreamItem(GObject.Object):
         return f"index: {self.index}, lang: {self.language}, is_default: {self.is_default}, title: {self.title}"
 
     def get_display_time(self):
-        return f"{self.duration_text} | {self.duration_in_seconds} s"
+        return f"{Formatter.format_time(self.duration_text)} | {Formatter.format_time(self.duration_in_seconds)} s"
+
+    def get_display_frame_rate(self):
+        return f"{Formatter.format_time(self.avg_frame_rate)}"
 
     def is_audio(self):
         return self.codec_type == "audio"
@@ -69,8 +75,44 @@ class VideoItem(GObject.Object):
             return False
         return len([s for s in self.streams if s.is_audio()]) > 1
 
+    def get_video_stream(self):
+        return next((s for s in self.streams if s.is_video()), None)
+
     def get_audio_streams(self):
         return [s for s in self.streams if s.is_audio()]
 
     def __str__(self):
         return f"VideoItem{{ name: {self.name}; audio_index_selected: {self.audio_stream_index_selected} }}"
+
+
+class Formatter:
+
+    @staticmethod
+    def format_number(number) -> str:
+        """
+        Formatea un número a 2 decimales.
+        """
+        value = Decimal(str(number))
+        return str(value.quantize(Decimal("0.01"), rounding=ROUND_HALF_UP))
+
+    @staticmethod
+    def format_duration(duration: str) -> str:
+        """
+        Formatea una duración HH:MM:SS.xxxxx a HH:MM:SS.xx
+        """
+        hms, fraction = duration.split(".")
+        decimals = Formatter.format_number(f"0.{fraction}").split(".")[1]
+
+        return f"{hms}.{decimals}"
+
+    @staticmethod
+    def format_time(value) -> str:
+        """
+        Detecta automáticamente si es una duración o un número.
+        """
+        value = str(value)
+
+        if ":" in value:
+            return Formatter.format_duration(value)
+
+        return Formatter.format_number(value)
